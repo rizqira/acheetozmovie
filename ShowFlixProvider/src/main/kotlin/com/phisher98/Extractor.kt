a0f5fadca6321c55c0830e7b6dc752504c4f4e77
rizqira <rizqirmdhqn@gmail.com>
2025-06-25 12:10:39 +0700

    .

--- ShowFlixProvider/src/main/kotlin/com/phisher98/Extractor.kt
+++
@@ -1,9 +0,0 @@
-package com.phisher98
-
-import com.lagradost.cloudstream3.extractors.StreamWishExtractor
-
-open class StreamRuby : StreamWishExtractor() {
-    override val name = "StreamRuby"
-    override val mainUrl = "https://streamruby.com"
-    override val requiresReferer = false
-}
