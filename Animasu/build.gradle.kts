5f967aa7668db55935cdffa2fbc1f77b3517c89d
rizqira <rizqirmdhqn@gmail.com>
2025-06-25 22:00:11 +0700

    auah

--- Animasu/build.gradle.kts
+++ Animasu/build.gradle.kts
@@ -23,5 +23,5 @@ cloudstream {
         "Anime",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=animasu.cc&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=aci.cc&sz=%size%"
 }
--- Animasu/src/main/kotlin/com/aci/AnimasuPlugin.kt
+++ Animasu/src/main/kotlin/com/aci/AnimasuPlugin.kt
@@ -1,17 +1,18 @@
-
 package com.aci
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class AnimasuPlugin: Plugin() {
+class aciPlugin : Plugin() {
     override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
-        registerMainAPI(Animasu())
+        // All providers should be added in this manner. Please don't edit the providers list
+        // directly.
+        registerMainAPI(aci())
         registerExtractorAPI(Archivd())
         registerExtractorAPI(Newuservideo())
         registerExtractorAPI(Vidhidepro())
+        registerExtractorAPI(Blogger())
     }
 }
